840636abe6f59bb8ce577defdeb7bc8c5904c01f
keerthi6790 <balakeerthi2710@gmail.com>
2022-05-22 12:31:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import './App.css';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import About from './component1/about';
-import Home from './component1/home';
-import Product from './component1/product';
-import Contact from './component1/contact';
-import {route,link} from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./component1/about";
+import Home from "./component1/home";
+import Product from "./component1/product";
+import Contact from "./component1/contact";
 function App() {
   return (
-    // <Router>
-    <div className='nav1'>
-      <Home/>
-      <About/>
-      <Contact/>
-      <Product/>
-      
-  {/* <Route  path="/" component={Home}/>
-  <Route  path="/about" component={About}/>
-  <Route  path="/product" component={Product}/>
-  <Route  path="/contact" component={Contact}/> */}
-  
-    </div>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/product" element={<Product/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
   );
 }
 
